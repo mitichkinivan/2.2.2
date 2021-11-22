40529f1afa473b01be7d88f2522bb82f95cbbd52
mitichkinivan <mitichkin.ivan@gmail.com>
2021-11-23 00:25:31 +0700

    2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -2,28 +2,28 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDAO;
+
+
+import web.service.CarService;
 
 
 @Controller
-@RequestMapping("/cars")
 public class CarController {
 
-    @Autowired
-    private CarDAO carDAO;
+    private final CarService carService;
 
-    @GetMapping()
-    public String getCount(@RequestParam(defaultValue = "0") int count, Model model){
-        if (count == 0){
-            model.addAttribute("carList", carDAO.getCars());
-        } else {
-            model.addAttribute("carList", carDAO.getCarsCount(count));
-        }
-        return "cars/cars";
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
+
+    @GetMapping("/cars")
+    public String getCarsPageByCount(ModelMap model, @RequestParam(value = "count", required = false) Integer count) {
+        model.addAttribute("carList", carService.getCars(count));
+        return "cars";
+    }
 }
